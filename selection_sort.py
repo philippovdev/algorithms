271235ccc99ac4cd8d6ef616ce49eb4d4b90e897
andrewphilippov <a@fandb.pro>
2020-08-10 23:43:17 +0300

    Selection sort sample

---
+++ selection_sort.py
@@ -0,0 +1,17 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([5,7,6,5,3,1,2,89,19,78,2,46]))
