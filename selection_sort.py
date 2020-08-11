adcc06d60e069e8819da5a1447284970cd113873
andrewphilippov <a@fandb.pro>
2020-08-11 23:23:49 +0300

    styling fixes

--- selection_sort.py
+++ selection_sort.py
@@ -1,4 +1,4 @@
-def findSmallest(arr):
+def find_smallest(arr):
     smallest = arr[0]
     smallest_index = 0
     for i in range(1, len(arr)):
@@ -7,11 +7,13 @@ def findSmallest(arr):
             smallest_index = i
     return smallest_index
 
-def selectionSort(arr):
-    newArr = []
-    for i in range(len(arr)):
-        smallest = findSmallest(arr)
-        newArr.append(arr.pop(smallest))
-    return newArr
 
-print(selectionSort([5,7,6,5,3,1,2,89,19,78,2,46]))
+def selection_sort(arr) -> list:
+    new_arr: list = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
+print(selection_sort([5, 7, 6, 5, 3, 1, 2, 89, 19, 78, 2, 46]))
